733287aff6692f429e8017ba6d62c4ee1153b48d
Jakub Kropáček <kropikuba@gmail.com>
2021-02-09 00:02:07 +0100

    pridany zaklady

---
+++ cogs/admin.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["prefix"])
+    @commands.has_permissions(Administrator=True)
+    async def changeprefix(self, ctx, prefix: str):
+        self.bot.db.update_prefix(ctx.guild.id, prefix)
+        await ctx.send(f"Prefix se záhadně změnil na {prefix}")
+    
---
+++ database.py
@@ -0,0 +1,32 @@
+import sqlite3
+import os.path
+
+
+class db():
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "casosklad.db")
+
+    def __init__(self):
+        self.mydb = sqlite3.connect(self.db_path)
+        self.cursor = self.mydb.cursor()
+
+    def fetch_prefix(self, gid):
+        prefix_query = "SELECT prefix FROM servers WHERE id = ?"
+        self.cursor.execute(prefix_query, (gid,))
+        prefix = self.cursor.fetchone()
+        return prefix[0]
+
+    def add_prefix(self, gid, prefix="."):
+        prefix_query = "INSERT INTO servers(id, prefix) VALUES (?, ?)"
+        self.cursor.execute(prefix_query, (gid, prefix,))
+        self.mydb.commit()
+
+    def remove_prefix(self, gid):
+        prefix_query = "DELETE FROM servers WHERE id = ?"
+        self.cursor.execute(prefix_query, (gid,))
+        self.mydb.commit()
+
+    def update_prefix(self, gid, prefix):
+        prefix_query = "UPDATE servers SET prefix = ? WHERE id = ?"
+        self.cursor.execute(prefix_query, (prefix, gid, ))
+        self.mydb.commit()
