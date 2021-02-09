da260d2b2e91ed60d199ca8def144e040ed84731
Jakub Kropáček <kropikuba@gmail.com>
2021-02-09 22:00:27 +0100

    F

---
+++ cogs/random.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+import discord
+
+
+class Random(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="f")
+    async def _sendF(self, ctx):
+        message = await ctx.send("Press F to pay respect")
+        await message.add_reaction(":regional_indicator_f:")
+
+
+def setup(bot):
+    bot.add_cog(Random(bot))
