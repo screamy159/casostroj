9e92922b3ad881ffd4f0bca3d8082fa9e151d99d
Jakub Kropáček <kropikuba@gmail.com>
2021-04-21 21:35:26 +0200

    Added requirements and dotenv

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import discord
 from discord.ext import commands
 import database
-import os.path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-token_path = os.path.join(BASE_DIR, "token.txt")
 
 
 def get_prefix(bot, message):
     if not message.guild:
         return '.'
     return bot.db.fetch_prefix(message.guild.id)
-
-
-def get_token():
-    with open(token_path, "r") as fp:
-        x = fp.readline()
-    return x
 
 
 cogs = ["admin", "random", "quotes", "polls"]
@@ -74,4 +69,4 @@ async def reload(ctx, extension):
     print(extension, "reloaded!")
 
 bot.db = database.db()
-bot.run(get_token())
+bot.run(os.environ["TOKEN"])
